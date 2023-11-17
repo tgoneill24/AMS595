249b019b0e6716f9e7d17cd1666b031be3c6f164
Thomas O'Neill <thomas.g.oneill@stonybrook.edu>
2023-11-17 18:15:28 -0500

    Added comments

--- CPPProject1/Project1Q4.cpp
+++ CPPProject1/Project1Q4.cpp
@@ -13,7 +13,7 @@ std::vector<int> prime_factorize(int n) {
     std::vector<int> answer;
     bool prime = true;
     
-    // Iterate through all integers (n0t including 1) less than or equal to n. Smallest one that divides n must be a prime.
+    // Iterate through all integers (not including 1) less than or equal to n. Smallest one that divides n must be a prime.
     // Add this one to the prime factorization, then add the prime factors of n divided by this integer.
     
     for (int i = 2; i <= n; i++) {
@@ -30,6 +30,8 @@ std::vector<int> prime_factorize(int n) {
     return answer;
 }
 
+
+// Test function
 void test_prime_factorize() {
     print_vector(prime_factorize(2));
     print_vector(prime_factorize(72));
